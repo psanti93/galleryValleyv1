958edb44af78a2efb17afce7f5e5255f535f3f4c
Paul Santiago <paulsantiago282@gmail.com>
2023-07-03 17:58:50 -0400

    Getting Query parameters

--- controllers/users.go
+++ controllers/users.go
@@ -12,21 +12,17 @@ type Users struct {
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	// we need a view to render
-	u.Templates.View.Execute(w, nil)
+	var data struct {
+		Email    string
+		Password string
+	}
+
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+	u.Templates.View.Execute(w, data)
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	// err := r.ParseForm()
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// }
-
-	/** one way to retrieve from input fields
-		fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
-	    fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
-
-	**/
 
 	fmt.Fprint(w, "Email: ", r.FormValue("email"))
 	fmt.Fprint(w, "Password: ", r.FormValue("password"))
