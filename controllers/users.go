2f5832e7c679cd5b499923f8cf1efa1e89e01519
Paul Santiago <paulsantiago282@gmail.com>
2023-07-03 17:40:18 -0400

    inital parsing form

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -13,4 +14,20 @@ type Users struct {
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	// we need a view to render
 	u.Templates.View.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// }
+
+	/** one way to retrieve from input fields
+		fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
+	    fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
+
+	**/
+
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))
+	fmt.Fprint(w, "Password: ", r.FormValue("password"))
 }
--- main.go
+++ main.go
@@ -31,6 +31,9 @@ func main() {
 	usersC.Templates.View = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
 
+	//testing out parsing sign up form
+	r.Post("/users", usersC.Create)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
