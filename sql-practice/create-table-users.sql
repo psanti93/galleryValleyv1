9f6628e3cea7d853bf4eb8db5ff02d4f457ae3ff
Paul Santiago <paulsantiago282@gmail.com>
2023-07-06 18:07:52 -0400

    Update creating tables notes and add inserting records into table

--- sql-practice/create-table-users.sql
+++ sql-practice/create-table-users.sql
@@ -1,7 +1,13 @@
 CREATE TABLE users (
-    id SERIAL PRIMARYER KEY,
+    id SERIAL PRIMARY KEY,
     age INT,
     first_name TEXT,
     last_name TEXT,
     email TEXT UNIQUE NOT NULL
 );
+
+Result:
+galleyvalley=# SELECT * FROM users;
+ id | age | first_name | last_name | email 
+----+-----+------------+-----------+-------
+(0 rows)
