6a5340db766a8574989fba7c0d38ecf9bba77331
Paul Santiago <paulsantiago282@gmail.com>
2023-07-19 23:35:29 -0400

    Add session service into user controller

--- controllers/users.go
+++ controllers/users.go
@@ -12,7 +12,8 @@ type Users struct {
 		SignUp View //using the view interface rather than reyling on the views.Templates package
 		SignIn View
 	}
-	UserService *models.UserService
+	UserService    *models.UserService
+	SessionService *models.SessionService
 }
 
 // Signing Up a New User
@@ -36,9 +37,24 @@ func (u Users) CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
+	session, err := u.SessionService.Create(user.ID)
 
-	// +v adds fields for users
-	fmt.Fprintf(w, "User Created:%+v ", user)
+	if err != nil {
+		fmt.Println(err)
+		// TODO: LONG term - show a warning that we're not able to sign a user in
+		http.Redirect(w, r, "/signin", http.StatusFound)
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:     "session",
+		Value:    session.Token,
+		Path:     "/",
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/users/me", http.StatusFound)
 }
 
 // Signing in Functionalities
@@ -69,28 +85,45 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	session, err := u.SessionService.Create(user.ID)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "User doesn't exist", http.StatusInternalServerError)
+		return
+	}
 	// creating a cookie with golang
 	cookie := http.Cookie{
-		Name:     "CookiePracticeEmail",
-		Value:    user.Email,
+		Name:     "session",
+		Value:    session.Token,
 		Path:     "/",
 		HttpOnly: true, // only want cookies to be accessible via http browser requests, don't allow cookies to work for java script
 	}
 	http.SetCookie(w, &cookie)
 
-	fmt.Fprintf(w, "User Authenticated: %+v", user)
+	http.Redirect(w, r, "/users/me", http.StatusFound)
 }
 
 // reading a cookie with golang
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
-	email, err := r.Cookie("CookiePracticeEmail")
+
+	tokenCookie, err := r.Cookie("session")
 
 	if err != nil {
-		fmt.Fprint(w, "The email cookie could not be read")
+		fmt.Println(err)
+		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
 
-	fmt.Fprintf(w, "Email cookie: %s\n", email.Value)
-	fmt.Fprintf(w, "Headers: %+v\n", r.Header)
+	// uses the user function from session service to look up the user based on that cookie
+	user, err := u.SessionService.User(tokenCookie.Value)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/signin", http.StatusFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Current user: %s\n", user.Email)
 
 }
