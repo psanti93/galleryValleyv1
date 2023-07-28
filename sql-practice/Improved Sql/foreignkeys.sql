4db6026613ca243bd5cf4dbeacc5bee7f21a52a6
Paul Santiago <paulsantiago282@gmail.com>
2023-07-28 12:56:28 -0400

    foreign key notes

---
+++ sql-practice/Improved Sql/foreignkeys.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE REFERENCES users (id) , --one option of creating foreign keys
+    token_hash TEXT UNIQUE NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users (id) --second option
+);
+
+-- will tell our db that users id are entered intot eh sessions table map to real users objects
+-- if we delete a user they can't have a session associated with them
