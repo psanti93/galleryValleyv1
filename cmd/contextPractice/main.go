c15967f9a1b5ab9089251d1edeec5ce1c5309856
Paul Santiago <paulsantiago282@gmail.com>
2023-08-28 14:03:37 -0400

    add example of setting context with context types

--- cmd/contextPractice/main.go
+++ cmd/contextPractice/main.go
@@ -5,11 +5,19 @@ import (
 	"fmt"
 )
 
+// type should never be exported
+type ctxKey string
+
+const (
+	// keys should never be exported
+	favoriteColorKey ctxKey = "favorite-color"
+)
+
 func main() {
 	// creates a new context
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, "Favorite-Color", "blue")
-	value := ctx.Value("Favorite-Color")
-
+	ctx = context.WithValue(ctx, favoriteColorKey, "blue")
+	//value := ctx.Value("favorite-color") // would return nil because "favorite-color" is of type string and not type ctx key
+	value := ctx.Value(favoriteColorKey)
 	fmt.Printf("Value : %v", value)
 }
