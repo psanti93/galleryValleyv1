48dfc027e38dc7aa00cf7a5fa8cc1a7c7e68a6e8
Paul Santiago <paulsantiago282@gmail.com>
2023-12-19 17:02:17 -0500

    Complete email service to send email, and to setFrom

--- models/email.go
+++ models/email.go
@@ -1,6 +1,18 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+
+	"github.com/go-mail/mail/v2"
+)
+
+type Email struct {
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
+}
 
 type SMTConfig struct {
 	Host     string
@@ -26,4 +38,40 @@ func NewEmailService(config SMTConfig) *EmailService {
 	}
 
 	return &es
+}
+
+func (es *EmailService) Send(email Email) error {
+	msg := mail.NewMessage()
+	msg.SetHeader("To", email.To)
+	es.setFrom(msg, email) // Set FROM field to a default value if it's not set in the email
+	msg.SetHeader("Subject", email.Subject)
+	switch {
+	case email.Plaintext != "" && email.HTML != "":
+		msg.SetBody("text/plain", email.Plaintext)
+		msg.AddAlternative("text/html", email.HTML)
+	case email.Plaintext != "":
+		msg.SetBody("text/plain", email.Plaintext)
+	case email.HTML != "":
+		msg.SetBody("text/html", email.HTML)
+	}
+
+	err := es.dailer.DialAndSend(msg)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
+
+func (es EmailService) setFrom(msg *mail.Message, email Email) {
+	var from string
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+
+	msg.SetHeader("From", from)
 }
